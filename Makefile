53ced88698baee164e590858cea149f4a486e3d3
raahul14 <rahul.mittal1499@gmail.com>
2019-04-16 18:51:32 -0400

    Pho break

---
+++ Makefile
@@ -0,0 +1,4 @@
+NVCC=/usr/local/cuda-9.1/bin/nvcc
+
+funcuit: funcuit.cu
+	${NVCC} -arch=sm_37 funcuit.cu -o funcuit 
--- funcuit.cu
+++ funcuit.cu
@@ -1,20 +1,62 @@
 #include <sys/time.h>
 #include <stdio.h>
 
-long N = 64000000;  // Play with this value                                                                                                                                                                                        
-int doPrint = 0; 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // GPU CODE
 //
-// Normal C function to square root values
-void normal(float* a, long N)                                                                                                                                                                                     
-{
-  long i;                                                                                                                                                                                                                
-  for (i = 0; i < N; ++i)                                                                                                                                                                                    
-    a[i] = sqrt(a[i]);                                                                                                                                                                                           
-}                 
+            
 
+//Normal C function to convert equation into tree
+void normal(char* a, long N) //a is the array, N is the size of the array
+{
+
+  
+		char input[] = "AB+CD+abd+jbn+";
+                             //[2,5,9,13];
+    int indexes [sizeof(input)/2];                
+		int count = 0;
+    int index = 0;
+		struct t
+    { 
+      struct t *OR;
+
+    }
+long i = 0;
+ for(i = 0; i < sizeof(input); i++)
+		{
+                    
+                    //'h' 'e' '+' 'l' 'l' '+'
+                                          //count = 2, i = 5;
+			if (input[i] != '+')
+			{
+				count++;
+                                indexes[index] = i;
+                                index++;
+			}
+			else
+			{
+                            //send to gpu child
+                                //loop through indexes array
+                                    //if i = 0, send 0 - i-1
+                                    //else send indexes[i]+1 to indexes[i+1]-1 
+                                //send OR
+                                                       
+                          //  struct t AND*;
+                            //OR.addChild(AND);
+                   
+                            for (int c = count; c > 0; c--)
+                            {
+                                AND.addChild(new GenericTreeNode<Character>(inputArray[i-c]));
+                            }
+                            count = 0;
+                            
+                            System.out.println(AND.toStringVerbose());
+			}
+		}
+}
+
+//Normal C function to co\
 // GPU function to square root values
 __global__ void gpu_sqrt(float* a, long N) {
    long element = blockIdx.x*blockDim.x + threadIdx.x; // Each thread must get a different element
